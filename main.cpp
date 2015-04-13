b82db2ed9856baf9a63693ba828f2a0683b86df4
Sweta23 <udayasree23@gmail.com>
2015-04-13 07:59:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 uint32_t temp;
 
 int main(void) {
-    GPIO_PWRUP -> PCONP |=(1<<15);
-    LPC_GPIO3->FIODIR|=1<<26;
+    GPIO_PWRUP -> PCONP |=(1<<15);   //power up the GPIO pins
+    LPC_GPIO3->FIODIR|=1<<26;        //Configure the direction to the pin 26 of port 3
     while(1) {
-         LPC_GPIO3->FIOPIN|=1<<26;
+         LPC_GPIO3->FIOPIN|=1<<26;    //toggle the pin by setting the PIN high and low
         //myled = 1;
         wait(0.2);
          LPC_GPIO3->FIOPIN &=~(1<<26);
